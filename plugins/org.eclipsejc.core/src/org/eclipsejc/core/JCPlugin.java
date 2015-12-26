34d24edb315605bbe5ce727ad250b376c4b6100b
Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>
2015-12-26 10:49:08 +0300

    core: add empty core plugin
    Signed-off-by: Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>

---
+++ plugins/org.eclipsejc.core/src/org/eclipsejc/core/JCPlugin.java
@@ -0,0 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Dmitry Eremin-Solenikov
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+package org.eclipsejc.core;
+
+import org.eclipse.core.runtime.Plugin;
+
+public class JCPlugin extends Plugin {
+
+	public static final String ID_PLUGIN = "org.eclipsejc.core";
+}
